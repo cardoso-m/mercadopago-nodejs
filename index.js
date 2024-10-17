b5c1f026ed0c22081925618cabdd2eb8210baeb1
Matheus <mdonizetecardoso@gmail.com>
2024-10-17 12:40:21 -0300

    Configurando o acess_token da API do mercado pago

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const mp = require('mercadopago')
 const app = express()
 
+const MercadoPeago = new mp.MercadoPagoConfig({
+    accessToken: "TEST-8133617760022540-101710-09d9f2be6e627583815a79374db0ef51-136059490"
+})
 
+app.get('/', (req, res) => {
+    res.send("API RODANDO!")
+})
 
 app.listen('8000', () =>{
     console.log('Server ON!')
